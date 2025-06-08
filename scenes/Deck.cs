bf9d3bf00d1ee02b01b99474ca1ffe3ec5b96bac
Evan Wildenhain <evan.wildenhain@gmail.com>
2025-06-07 19:25:43 -0700

    make deck bigger if there are more cards left

--- scenes/Deck.cs
+++ scenes/Deck.cs
@@ -8,9 +8,6 @@ using System.Collections.Generic;
 /// </summary>
 public partial class Deck : Control
 {
-  // Number of visual card backs to show in the stack
-  [Export] public int StackSize { get; set; } = 5;
-
   // Visual offset between stacked cards
   [Export] public Vector2 StackOffset { get; set; } = new Vector2(2, 2);
 
@@ -30,6 +27,9 @@ public partial class Deck : Control
   // The actual deck of cards
   private Stack<CardData> _cardDeck = new Stack<CardData>();
 
+  // Node to hold the visual card backs
+  private Control _visualStack;
+
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
   {
@@ -46,37 +46,94 @@ public partial class Deck : Control
     CustomMinimumSize = new Vector2(100, 150); // Same size as cards
     Size = CustomMinimumSize;
 
-    // Create the visual stack of card backs
-    CreateVisualStack();
+    // Create container for visual stack
+    _visualStack = new Control
+    {
+      LayoutMode = 1,
+      AnchorsPreset = (int)LayoutPreset.FullRect,
+      MouseFilter = MouseFilterEnum.Ignore
+    };
+    AddChild(_visualStack);
 
     // Initialize and shuffle the deck
     InitializeDeck();
+
+    // Make the entire deck clickable
+    MouseDefaultCursorShape = CursorShape.PointingHand;
   }
 
   /// <summary>
-  /// Creates the visual representation of stacked cards
+  /// Updates the visual representation of the deck based on number of cards remaining
   /// </summary>
-  private void CreateVisualStack()
+  private void UpdateVisualStack()
   {
-    // Create card backs from bottom to top
-    for (int i = 0; i < StackSize; i++)
+    // Remove all existing visual elements
+    foreach (var child in _visualStack.GetChildren())
     {
-      var cardBack = new ColorRect
-      {
-        LayoutMode = 1, // Use anchors/containers layout mode
-        CustomMinimumSize = Size,
-        Size = Size,
-        Color = new Color(1, 0, 0, 0.5f), // Same red as card back
-        MouseFilter = MouseFilterEnum.Ignore,
-        Position = StackOffset * i,
-      };
-
-      // Add the card back to the scene
-      AddChild(cardBack);
+      child.QueueFree();
     }
 
-    // Make the entire stack clickable
-    MouseDefaultCursorShape = CursorShape.PointingHand;
+    if (_cardDeck.Count == 0)
+    {
+      // Create empty deck placeholder with dashed border
+      var emptyDeck = new ColorRect
+      {
+        LayoutMode = 1,
+        AnchorsPreset = (int)LayoutPreset.FullRect,
+        Color = new Color(0, 0, 0, 0), // Transparent
+        MouseFilter = MouseFilterEnum.Ignore
+      };
+
+      // Add a border line to create a dashed effect
+      var border = new Line2D
+      {
+        DefaultColor = Colors.Gray,
+        Width = 2,
+        Closed = true
+      };
+      border.AddPoint(new Vector2(0, 0));
+      border.AddPoint(new Vector2(Size.X, 0));
+      border.AddPoint(new Vector2(Size.X, Size.Y));
+      border.AddPoint(new Vector2(0, Size.Y));
+      border.DefaultColor = Colors.Gray;
+      border.Width = 2;
+      border.Closed = true;
+
+      // Set the line to be dashed
+      var dashLength = 5;
+      var gapLength = 5;
+      var pattern = new float[] { dashLength, gapLength };
+      border.SetMeta("_edit_lock_", true);
+      border.Width = 2;
+      border.DefaultColor = Colors.Gray;
+      border.JointMode = Line2D.LineJointMode.Round;
+      border.BeginCapMode = Line2D.LineCapMode.Round;
+      border.EndCapMode = Line2D.LineCapMode.Round;
+
+      emptyDeck.AddChild(border);
+      _visualStack.AddChild(emptyDeck);
+    }
+    else
+    {
+      // Calculate number of visual cards to show (1 per 10 cards, rounding up)
+      int visualCards = (_cardDeck.Count + 9) / 10; // Integer division rounding up
+
+      // Create card backs from bottom to top
+      for (int i = 0; i < visualCards; i++)
+      {
+        var cardBack = new ColorRect
+        {
+          LayoutMode = 1,
+          CustomMinimumSize = Size,
+          Size = Size,
+          Color = new Color(1, 0, 0, 0.5f), // Same red as card back
+          MouseFilter = MouseFilterEnum.Ignore,
+          Position = StackOffset * i
+        };
+
+        _visualStack.AddChild(cardBack);
+      }
+    }
   }
 
   /// <summary>
@@ -109,6 +166,9 @@ public partial class Deck : Control
     {
       _cardDeck.Push(card);
     }
+
+    // Update the visual representation
+    UpdateVisualStack();
 
     GD.Print($"Deck initialized with {_cardDeck.Count} cards");
   }
@@ -155,6 +215,9 @@ public partial class Deck : Control
 
     // Keep track of the spawned card
     _spawnedCards.Add(card);
+
+    // Update the visual representation
+    UpdateVisualStack();
 
     GD.Print($"Drew {cardData}, {_cardDeck.Count} cards remaining");
   }
